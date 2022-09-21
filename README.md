0ee7f43e2d1e9e3191fbdc81faf6c6ca3626dd1a
harshit <harshit.tiwari2020@vitbhopal.ac.in>
2022-09-21 10:38:03 +0530

    middle commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# IASpiration
+
+---
+1. go to the directory
+2. open gitbash. 
+```
+3. cmd: git clone url (paste the url of thir repo).
+```
+```
+git clone git@github.com:harshitisback/IASpiration.git
+```
+---
+
+
+> every time to update repo in directory before start code 
+> 1. open gitbash. 
+```
+> 2. cmd: git pull (so it will update the directory with current repo)
+```
---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static(__dirname+'/public'));
+
+
+app.get("/", function (req, res) {
+    res.render("home");
+
+});
+
+
+app.listen(3000, function () { 
+    console.log("server started at port 1000");
+ })
